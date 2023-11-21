108462731c2cb9634b3fe8fbbb87340dceed6d8f
TheRivi <rivaldoreid123@gmail.com>
2023-11-21 04:09:53 -0500

    Edited files

---
+++ world.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$username = 'lab5_user';
+$password = 'password123';
+$dbname = 'world';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$stmt = $conn->query("SELECT * FROM countries");
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$country = $_GET['country'];
+if(isset($_GET['lookup'])&& $_GET['lookup'] === 'cities'){
+  $stmt = $conn->query("SELECT * FROM countries cty join cities c on c.country_code = city.code WHERE ")
+}
+?>
+<ul>
+<?php foreach ($results as $row): ?>
+  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php endforeach; ?>
+</ul>
